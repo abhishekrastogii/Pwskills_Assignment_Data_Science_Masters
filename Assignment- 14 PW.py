0223da5f1197c597e9df59e8eb7181467d74ed9d
ABHISHEK RASTOGI <abhishekrastogi8532@gmail.com>
2023-06-24 17:14:52 +0530

    Add files via upload

---
+++ Assignment- 14 PW.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+Q1. What is an API? Give an example, where an API is used in real life.
+
+Q2. Give advantages and disadvantages of using API. 
+
+Q3. What is a Web API? Differentiate between API and Web API.
+
+Q4. Explain REST and SOAP Architecture. Mention shortcomings of SOAP.
+
+Q5. Differentiate between REST and SOAP
+
+
+# In[ ]:
+
+
+Q1. What is an API? Give an example, where an API is used in real life.
+
+
+# In[ ]:
+
+
+A1. API is an abbreviation for Application Programming Interface which is a collection of communication protocols and 
+    subroutines used by various programs to communicate between them. A programmer can make use of various API tools to
+    make their program easier and simpler. Also, an API facilitates programmers with an efficient way to develop their 
+    software programs. Thus in simpler terms, an API helps two programs or applications to communicate with each other by
+    providing them with the necessary tools and functions. It takes the request from the user and sends it to the service
+    provider and then again sends the result generated from the service provider to the desired user. 
+
+
+# In[ ]:
+
+
+Q2. Give advantages and disadvantages of using API. 
+
+
+# In[ ]:
+
+
+A2. There is a lot of conveniences and advantages to APIs, but business leaders should also be aware of the disadvantages.
+    As a single point of entry, an API is a gateway and can become a hacker's primary target. Once the API is compromised,
+    all other applications and systems become vulnerable.
+
+
+# In[ ]:
+
+
+Q3. What is a Web API? Differentiate between API and Web API.
+
+
+# In[ ]:
+
+
+A3. APIs are application interfaces, meaning that one application is able to interact with another application in a 
+    standardized way. Web services are a type of API, which must be accessed through a network connection. REST APIs are 
+    a standardized architecture for building web APIs using HTTP methods.
+
+
+# In[ ]:
+
+
+Q4. Explain REST and SOAP Architecture. Mention shortcomings of SOAP.
+
+
+# In[ ]:
+
+
+A4. SOAP does not support caching API calls. SOAP is much more complicated than REST, which can have performance 
+    implications. SOAP is much less adaptable than REST. SOAP is usually slower than REST.
+
+
+# In[ ]:
+
+
+Q5. Differentiate between REST and SOAP
+
+
+# In[ ]:
+
+
+A5. SOAP is a protocol. REST is an architectural style. SOAP stands for Simple Object Access Protocol.
+    REST stands for REpresentational State Transfer. SOAP can't use REST because it is a protocol. 
+    REST can use SOAP web services because it is a concept and can use any protocol like HTTP, SOAP. 
+    SOAP uses services interfaces to expose the business logic.
+
